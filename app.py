ffa73b0d4a803ab26549ae2591cc037bf4310c42
Marko <markogidej@yahoo.com>
2024-07-17 11:13:33 -0400

    Connection working. Customer show on load

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from marshmallow import fields, validate, ValidationError
 from my_password import password
+from flask_cors import CORS
 
 db_password = password
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{db_password}@localhost/e_commerce_db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+CORS(app)
 
 class CustomerSchema(ma.Schema):
     name = fields.String(required=True)
